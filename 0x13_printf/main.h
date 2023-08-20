0728efb8c6f608d77c7de7f1ee69f85a4ee84e3a
Victor <victormwendwa804@gmail.com>
2023-08-20 04:06:56 -0700

    Added directories

---
+++ 0x13_printf/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>  // Include necessary headers
+
+/* Declare function prototypes */
+int _printf(const char *format, ...);
+void otherFunction(void); // Example of another function
+
+/* Define macros */
+#define BUFFER_SIZE 1024
+
+#endif /* MAIN_H */
